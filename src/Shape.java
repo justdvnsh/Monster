cae22b1b3eb0bd0d3f6d872a796e4d31df78701e
justdvnsh <justdvnsh2208@gmail.com>
2019-09-12 16:48:16 +0530

    number shapews

--- src/Shape.java
+++ src/Shape.java
@@ -2,39 +2,57 @@ import java.util.Scanner;
 
 public class Shape {
 
-    Scanner userInput = new Scanner(System.in);
+    static Scanner userInput = new Scanner(System.in);
     public static void main(String args[]) {
+
+        int number = userInput.nextInt();
+
+        double numberDouble = (double) number;
+
+        Numbers num = new Numbers();
+
+        boolean isSquare = num.getSquare(numberDouble);
+        boolean isTriangular = num.getTriangular(numberDouble);
+
+        if ( isSquare & isTriangular ) {
+            System.out.println(" The number is square and triangular. ");
+        } else if ( isTriangular & !isSquare) {
+            System.out.println(" The number is triangular only. ");
+        } else if ( isSquare & !isTriangular ) {
+            System.out.println(" The number is square only. ");
+        } else {
+            System.out.println(" The number is neither triangular nor square. ");
+        }
 
     }   
 }
 
-public class Numbers {
+class Numbers {
 
-    int number;
-
-    public Numbers(int number) {
-        this.number = number;
-    }
-
-    public boolean getSquare() {
+    public boolean getSquare(double number) {
         
         // square numbers are those which are complete multiples of themselves
         // eg. 4x4 = 16, so 16 is sqaure number. Perfect square
 
-        double sqrt = Math.sqrt(number);
-
-        return (( srqt - Math.floor(sqrt) ) == 0)
+        return ( Math.sqrt( number ) == (int) Math.sqrt( number ) );
 
     }
 
-    public boolean getTriangular() {
+    public boolean getTriangular(double number) {
 
         // triagnular numbers are those , which can be calculated as
         // n = m(m+1)/2 , where m is the position of the element.
         // so if we want to find the 3rd triangular number , it will be
         // 3(3+1)/2 = 6.  
 
-        double sqrt = 8*number - 1;
+        System.out.println(" Before the method-  " + number);
+
+        double sqr = 8*number + 1;
+
+        System.out.println(" After the method- " + number);
+
+        return ( this.getSquare(sqr) );
+
     }
 
 }
